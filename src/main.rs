2d23cbc81cdb4f9288075b1e9c43e6860caa0a2a
Sitong <sitongpeng@gmail.com>
2024-05-16 16:50:44 -0400

    add camera origin, viewport, and rays onto viewport

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 mod vec;
+mod ray;
+
 use std::io::{stderr, Write};
-use vec::Color;
+use vec::{Color, Point3, Vec3};
+use ray::Ray;
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_ray = r.direction().normalized();
+    let t = 0.5 * (unit_ray.y() + 1.0);
+
+    // hard code a linear interpolation from white to blue
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -15,11 +38,14 @@ fn main() {
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                0.25
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical
             );
+            let pixel_color = ray_color(&r);
 
             println!("{}", pixel_color.format_color());
         }

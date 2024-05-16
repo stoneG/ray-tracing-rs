af99a97cf4bc64f77d9516933e1c737fac4c0ceb
Sitong <sitongpeng@gmail.com>
2024-05-16 13:28:51 -0400

    add generic Vec3 point implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+mod vec;
 use std::io::{stderr, Write};
+use vec::Color;
 
 fn main() {
     const IMAGE_WIDTH: u64 = 256;
@@ -13,15 +15,13 @@ fn main() {
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b = 0.25;
+            let pixel_color = Color::new(
+                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
+                (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
+                0.25
+            );
 
-            let ir = (255.999 * r) as u64;
-            let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b) as u64;
-
-            println!("{} {} {}", ir, ig, ib);
+            println!("{}", pixel_color.format_color());
         }
     }
     eprintln!("Done.");

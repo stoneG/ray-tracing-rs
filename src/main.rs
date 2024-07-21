d7ab2c581898a1d405f4fe3d0fa23c9a9caa605b
Sitong <sitongpeng@gmail.com>
2024-07-21 19:41:56 -0400

    add dielectric (clear) material with refracting

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use ray::Ray;
 use hit::{Hit, World};
 use sphere::Sphere;
 use camera::Camera;
-use material::{Lambertian, Metal};
+use material::{Dielectric, Lambertian, Metal};
 
 fn ray_color(ray: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
@@ -46,9 +46,9 @@ fn main() {
     // World
     let mut world = World::new();
     let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.1));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let mat_left = Rc::new(Dielectric::new(1.5));
+    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
     let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
--- src/material.rs
+++ src/material.rs
@@ -63,3 +63,39 @@ impl Scatter for Metal {
         }
     }
 }
+
+pub struct Dielectric {
+    ir: f64
+}
+
+impl Dielectric {
+    pub fn new(index_of_refraction: f64) -> Dielectric {
+        Dielectric {
+            ir: index_of_refraction
+        }
+    }
+}
+
+impl Scatter for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let refraction_ratio = if rec.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction = r_in.direction().normalized();
+        let cos_theta = ((-1.0) * unit_direction).dot(rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+
+        let direction = if refraction_ratio * sin_theta > 1.0 {
+            unit_direction.reflect(rec.normal)
+        } else {
+            unit_direction.refract(rec.normal, refraction_ratio)
+        };
+
+        let scattered = Ray::new(rec.p, direction);
+
+        Some((Color::new(1.0, 1.0, 1.0), scattered))
+    }
+}
